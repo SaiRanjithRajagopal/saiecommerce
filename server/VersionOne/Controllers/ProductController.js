d876b717bbc1a87e7f0a77b2fb043a287dae28c3
sairanj <ranjith.rajagopal4u@gmail.com>
2021-02-21 20:00:17 -0500

    added the seeder configuration

--- server/VersionOne/Controllers/ProductController.js
+++ server/VersionOne/Controllers/ProductController.js
@@ -2,7 +2,6 @@ const Product = require('../Models/products');
 
 exports.newProduct = async (req, res, next) => {
     const product = await Product.create(req.body);
-
     res.status(201).json({
         success: true,
         product
