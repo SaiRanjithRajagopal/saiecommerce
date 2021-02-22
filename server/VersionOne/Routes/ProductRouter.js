411a9ac9859bde41326069eb6ec9c46228d1b2f9
sairanj <ranjith.rajagopal4u@gmail.com>
2021-02-21 19:39:59 -0500

    added the products router and create new product end points

--- server/VersionOne/Routes/ProductRouter.js
+++ server/VersionOne/Routes/ProductRouter.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 
-const { getProducts } = require('../Controllers/ProductController')
+const { getProducts, newProduct } = require('../Controllers/ProductController')
 
 router.route('/products').get(getProducts);
+router.route('/products/createNewProduct').post(newProduct);
 
 module.exports = router;
