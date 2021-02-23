343d61aad34ecb47b41ef1d35f90a5e5daa3f005
sairanj <ranjith.rajagopal4u@gmail.com>
2021-02-22 21:46:28 -0500

    Added the logic for validation the user name and password

--- server/VersionOne/Controllers/UserController.js
+++ server/VersionOne/Controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require('../Models/user');
 
 const errorHandler = require('../../Utils/errorHandler');
 const catchAsyncError = require('../Middleware/asyncErrorMiddleware');
+const ErrorHandler = require('../../Utils/errorHandler');
 
 exports.registerUser = catchAsyncError(async (req, res, next) => {
     const { name, email, password } = req.body;
@@ -14,9 +15,35 @@ exports.registerUser = catchAsyncError(async (req, res, next) => {
             url: 'https://res.cloudinary.com/shopit/image/upload/v1606305757/avatars/kccvibpsuiusmwfepb3m.png'
         }
     });
-
     const token = user.getJwtToken();
-
     res.status(201).json({ "success": true, token })
 });
 
+
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new ErrorHandler('Please enter email or password', 400));
+    }
+
+    //Finding user in database
+    //We are eliminating the password in the selection
+    const user = await User.findOne({ email }).select('+password');
+
+    //If user not found return 401 error
+    if (!user) {
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    //Check if password is correct or not
+    const isPasswordMatched = await user.ComparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    const token = user.getJwtToken();
+
+    res.status(200).json({ "success": true, token })
+});
--- server/VersionOne/Routes/UserRouter.js
+++ server/VersionOne/Routes/UserRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser } = require('../Controllers/UserController')
+const { registerUser, loginUser } = require('../Controllers/UserController')
 
-router.route('/user/register').get(registerUser);
+router.route('/user/register').post(registerUser);
+router.route('/user/login').get(loginUser);
 
 module.exports = router;
