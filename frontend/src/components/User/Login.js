29197a74d887a808238fbec6a57d13cbf12d4a89
sairanj <ranjith.rajagopal4u@gmail.com>
2021-03-07 23:16:10 -0500

    completed the new user creation logic

--- frontend/src/components/User/Login.js
+++ frontend/src/components/User/Login.js
@@ -7,7 +7,7 @@ import './Login.css'
 
 import Spinner from '../Loader/Spinner'
 import { authenticateUser, clearErrors } from '../Redux_Thunk/Actions/UserAction'
-import { Link } from 'react-router-dom';
+
 
 const Login = ({ history }) => {
 
@@ -39,37 +39,35 @@ const Login = ({ history }) => {
     return (
         <React.Fragment>
             {loading ? <Spinner /> : (
-                <div className="text-center">
-                    <div className="form-signin">
-                        <form onSubmit={loginHandler}>
-                            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-                            <label htmlFor="inputEmail" className="visually-hidden">Email address</label>
+                <div className="form-signin text-center">
+                    <form onSubmit={loginHandler}>
+                        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+                        <label htmlFor="inputEmail" className="visually-hidden">Email address</label>
 
-                            <input type="email" id="inputEmail"
-                                className="form-control"
-                                placeholder="Email address" required
-                                value={userName}
-                                onChange={(e) => setUserName(e.target.value)}
-                            />
+                        <input type="email" id="inputEmail"
+                            className="form-control"
+                            placeholder="Email address" required
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
+                        />
 
-                            <label htmlFor="inputPassword" className="visually-hidden">Password</label>
+                        <label htmlFor="inputPassword" className="visually-hidden">Password</label>
 
-                            <input type="password"
-                                id="inputPassword"
-                                className="form-control"
-                                onChange={(e) => setPassword(e.target.value)}
-                                value={password}
-                                placeholder="Password" required />
-                            <button className="btn btn-success" type="submit">Sign in</button>
-                        </form>
-                        <br />
-                        <div className="row">
-                            <div className="col">
-                                <NavLink to="/" className="btn btn-primary" type="button">Password</NavLink>
-                            </div>
-                            <div className="col">
-                                <NavLink to="/User/Register" className="btn btn-warning" type="button">Register</NavLink>
-                            </div>
+                        <input type="password"
+                            id="inputPassword"
+                            className="form-control"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            placeholder="Password" required />
+                        <button className="btn btn-success" type="submit">Sign in</button>
+                    </form>
+                    <br />
+                    <div className="row">
+                        <div className="col">
+                            <NavLink to="/" className="btn btn-primary" type="button">Password</NavLink>
+                        </div>
+                        <div className="col">
+                            <NavLink to="/User/Register" className="btn btn-warning" type="button">Register</NavLink>
                         </div>
                     </div>
                 </div>
--- frontend/src/components/User/RegisterUser.js
+++ frontend/src/components/User/RegisterUser.js
@@ -1,10 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useAlert } from 'react-alert';
+import { NavLink } from 'react-router-dom';
 
-const RegisterUser = () => {
+import './Login.css'
+
+import Spinner from '../Loader/Spinner';
+import { registerUser, clearErrors } from '../Redux_Thunk/Actions/UserAction'
+
+const RegisterUser = ({ history }) => {
+
+    const dispatch = useDispatch();
+    const alert = useAlert();
+
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+
+    const { loading, user, error, newUserCreated } = useSelector(state => state.newUser);
+
+    useEffect(() => {
+        if (error) {
+            alert.error(error);
+            dispatch(clearErrors());
+        }
+    }, [dispatch, alert, error, newUserCreated]);
+
+    const registerNewUser = (e) => {
+        e.preventDefault();
+        dispatch(registerUser(userName, password, name));
+    }
+
     return (
-        <div>
-            <p>Register USer</p>
-        </div>
+        <React.Fragment>
+            {loading ? <Spinner /> : (
+                <div className="form-signin text-center">
+                    <form onSubmit={registerNewUser}>
+                        <h1 className="h3 mb-3 fw-normal">User Register Page</h1>
+
+                        <label htmlFor="inputEmail" className="visually-hidden">Email address</label>
+                        <input type="email" id="inputEmail"
+                            className="form-control"
+                            placeholder="Email address" required
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
+                        />
+
+                        <label htmlFor="inputPassword" className="visually-hidden">Password</label>
+                        <input type="password"
+                            id="inputPassword"
+                            className="form-control"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            placeholder="Password" required />
+
+                        <label htmlFor="inputName" className="visually-hidden">Name</label>
+                        <input type="text"
+                            id="inputName"
+                            className="form-control"
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}
+                            placeholder="Name" required />
+                        <br />
+
+                        <button className="btn btn-success" type="submit">Register</button>
+                    </form>
+                    <br />
+                </div>
+            )}
+        </React.Fragment>
     )
 }
 
